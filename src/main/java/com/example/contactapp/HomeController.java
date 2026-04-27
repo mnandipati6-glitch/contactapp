f63a3dcbf6cab3c3b53484fc568f31737660af69
ncmrdataengineer <ncmrdataengineer@gmail.com>
2026-04-27 16:29:30 -0700

    refactor: simplify HomeController by removing unused methods and imports

--- src/main/java/com/example/contactapp/HomeController.java
+++ src/main/java/com/example/contactapp/HomeController.java
@@ -1,28 +1,13 @@
 package com.example.contactapp;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    private List<Contact> contacts = new ArrayList<>();
-
     @GetMapping("/")
     public String home() {
-        return "Contact Manager Running 🚀";
-    }
-
-    @PostMapping("/add")
-    public String addContact(@RequestBody Contact contact) {
-        contacts.add(contact);
-        return "Contact Added";
-    }
-
-    @GetMapping("/contacts")
-    public List<Contact> getContacts() {
-        return contacts;
+        return "Contact Manager Running";
     }
 }
